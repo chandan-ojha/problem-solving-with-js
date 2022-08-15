a38271d0e65c202912ecf2b59ef04381595591d3
Chandan-Ojha <chandanojha776@gmail.com>
2022-08-15 17:34:36 +0600

    promises basic problem solved

--- Problem Solving_4/promise.js
+++ Problem Solving_4/promise.js
@@ -33,3 +33,67 @@ promise
 
 console.log('Task 3');
 
+
+/** Promise Basic Problem Solved **/
+const paymentSuccess = true;
+const marks = 80;
+
+
+function enroll(){
+   console.log('Course enrollment is is progress.....');
+
+   const promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        if(paymentSuccess) {
+            resolve();
+        }else {
+            reject('Payment failed');
+        }
+    }, 2000)
+
+   });
+   return promise;
+
+}
+
+function progress(){
+  console.log('Course on progress...');
+
+  const promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+      if(marks >= 80) {
+           resolve();
+      } else {
+           reject('You could not get enough marks to get the certificate');
+      }
+    }, 3000);
+
+  });
+
+  return promise;
+
+}
+
+function getCertificate(){
+  console.log('Preparing your certificate!');
+  
+  const promise = new Promise(function(resolve){
+    setTimeout(function(){
+       resolve("Congrats! You got the certificate")
+    }, 1000);
+
+  });
+
+  return promise;
+  
+}
+
+enroll()
+   .then(progress)
+   .then(getCertificate)
+   .then(function(value){
+      console.log(value);
+   })
+   .catch(function(err){
+      console.log(err); 
+   })
